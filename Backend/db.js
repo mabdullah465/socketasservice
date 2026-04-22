49773ee89cc67bd423256553137e261f223b81c3
Muhammad Abdullah <abdullah457tu@gmail.com>
2026-04-22 10:48:39 -0700

    updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,10 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "loremispum",
+  host: "mysql-34e4c26a-abdullah457tu-418e.i.aivencloud.com",
+  port:"21582",
+  user: "avnadmin",
+  password: "AVNS_dyWpB0sPm237dbewtU-",
   database: "socketservice",
 });
 
